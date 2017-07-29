5f3bf1c52838ee0999c63ab64cdf8888ddb91d0d
Wei-Chung Chen <wcchen101@gmail.com>
2017-07-28 19:49:41 -0700

    add contact list component

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5002/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5002/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5002/tyler.jpg"
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello world
+      	<ListContacts contacts={contacts} />
       </div>
       )
   }
